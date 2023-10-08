6f4f2c4c18cabdb2586a384cb6f3e958eff9440b
Lisa Smeke <lisasmeke@gmail.com>
2023-10-08 13:27:51 +0200

    Add responsive font to RSVP component

--- src/components/Rsvp.jsx
+++ src/components/Rsvp.jsx
@@ -6,10 +6,12 @@ const Rsvp = () => {
     <div className="rsvp">
       <div className="rsvp-text">
         <h2 className="rsvp-h2">¿Podrás acompañarnos?</h2>
-        <p className="rsvp-info">¡Te esperamos con toda la emoción y alegría del mundo!</p>
-        <p className="rsvp-info">
-          Por favor recuerda confirmar tu asistencia a través de WhatsApp.
-        </p>
+        <div className="rsvp-info-container">
+          <p className="rsvp-info">¡Te esperamos con toda la emoción y alegría del mundo!</p>
+          <p className="rsvp-info">
+            Por favor recuerda confirmar tu asistencia a través de WhatsApp.
+          </p>
+        </div>
       </div>
       <img className="rsvp-img" src={ns243} alt="Nadia y Stefan en Oaxaca"></img>
     </div>
