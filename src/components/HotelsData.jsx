f6d9faadbfb7ecb4faa0d9e046b423f91d69b2ed
Lisa Smeke <lisasmeke@gmail.com>
2023-10-29 10:05:31 +0100

    Reduce hotel img sizes

--- src/components/HotelsData.jsx
+++ src/components/HotelsData.jsx
@@ -1,15 +1,15 @@
 import hotel1 from '../assets/hotels/1-amomoxtli_small.jpg';
-import hotel2 from '../assets/hotels/2-hostal-luz.jpg';
+import hotel2 from '../assets/hotels/2-hostal-luz_small.jpg';
 import hotel3 from '../assets/hotels/3-casa-fernanda_small.jpg';
-import hotel4 from '../assets/hotels/4-posada-tepoz.jpg';
-import hotel5 from '../assets/hotels/5-buena-vibra.jpg';
-import hotel6 from '../assets/hotels/6-posada-valle.png';
-import hotel7 from '../assets/hotels/7-valle-mistico.jpg';
-import hotel8 from '../assets/hotels/8-quinta-gyo.jpg';
-import hotel9 from '../assets/hotels/9-amate-camp.jpg';
-import hotel10 from '../assets/hotels/10-real-valle.jpg';
-import hotel11 from '../assets/hotels/11-xacalan.jpg';
-import hotel12 from '../assets/hotels/12-pan-nuestro.jpg';
+import hotel4 from '../assets/hotels/4-posada-tepoz_small.jpg';
+import hotel5 from '../assets/hotels/5-buena-vibra_small.jpg';
+import hotel6 from '../assets/hotels/6-posada-valle_small.png';
+import hotel7 from '../assets/hotels/7-valle-mistico_small.jpg';
+import hotel8 from '../assets/hotels/8-quinta-gyo_small.jpg';
+import hotel9 from '../assets/hotels/9-amate-camp_small.jpg';
+import hotel10 from '../assets/hotels/10-real-valle_small.jpg';
+import hotel11 from '../assets/hotels/11-xacalan_small.jpg';
+import hotel12 from '../assets/hotels/12-pan-nuestro_small.jpg';
 
 export const hotelsData = [
   {
